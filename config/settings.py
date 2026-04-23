a92b150ebbd98c7f513ad6a149e65ee1934d6b9c
Bridger Bergeson <bridgerbergeson@u.boisestate.edu>
2026-04-22 18:07:41 -0600

    Fixed the favicon and settings

--- config/settings.py
+++ config/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # NEW: Tell Whitenoise where to gather all the files for production
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
